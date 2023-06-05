fa395a899922b15e952d9c9df8f1c33dea9e77ad
ElyaDzhy <elmira.freshcode@gmail.com>
2023-06-05 11:47:11 +0300

    Add JOIN tasks resolve

--- sql/join.session.sql
+++ sql/join.session.sql
@@ -59,3 +59,66 @@ SELECT *
 FROM chats
          JOIN users on users.id = chats.owner_id;
 
+---
+
+SELECT *
+FROM users
+         JOIN chats c on users.id = c.owner_id
+         JOIN messages m on c.id = m.chat_id;
+
+
+SELECT u.id         AS user_id,
+       u.first_name AS name,
+       c.id         AS chat_id
+FROM messages AS m
+         JOIN chats AS c ON m.chat_id = c.id
+         JOIN users u on u.id = m.author_id
+WHERE c.id = 1;
+
+-- все номера заказов, в которых были самсунги
+
+SELECT pto.order_id
+FROM products
+         JOIN products_to_orders AS pto ON products.id = pto.product_id
+WHERE brand = 'Samsung';
+
+-- группировка по моделям
+
+SELECT count(*), products.model
+FROM products
+         JOIN products_to_orders AS pto ON products.id = pto.product_id
+WHERE brand = 'Samsung'
+GROUP BY products.model;
+
+--
+SELECT count(*), brand
+FROM products
+         JOIN products_to_orders AS pto ON products.id = pto.product_id
+GROUP BY brand;
+---
+
+--- top 3 brand
+
+SELECT count(*) AS sales, brand
+FROM products
+         JOIN products_to_orders AS pto ON products.id = pto.product_id
+GROUP BY brand
+ORDER BY sales DESC
+LIMIT 3;
+
+--- top 5 models
+
+SELECT count(*) AS sales, model
+FROM products
+         JOIN products_to_orders AS pto ON products.id = pto.product_id
+GROUP BY model
+ORDER BY sales DESC
+LIMIT 5;
+
+
+-- maxprice-order
+SELECT *, (products.price * products_to_orders.quantity) AS "maxprice"
+FROM products_to_orders
+         JOIN products ON products_to_orders.product_id = products.id
+ORDER BY maxprice DESC
+LIMIT 1;
