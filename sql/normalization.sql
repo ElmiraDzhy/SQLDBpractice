3c78e70c3faa30725be3e2294cddd949fb0cb9ce
ElyaDzhy <elmira.freshcode@gmail.com>
2023-06-08 11:05:39 +0300

    Add example of BCNF

--- sql/normalization.sql
+++ sql/normalization.sql
@@ -98,3 +98,88 @@ INSERT INTO employees (name, department)
 VALUES ('John Doe', 'HR'),
        ('Jane Smith', 'Sales'),
        ('Andrew', 'HR');
+
+-- BCNF
+
+-- NOT IN BCNF
+--      ||
+--      \/
+
+/*
+Example:
+teachers, students, subjects
+
+Students can go to subjects
+One teacher can only teach one subjects
+Students can do into more than one subjects
+
+teachers n:1 subjects
+students m:n subjects
+students m:n teachers
+*/
+CREATE TABLE students
+(
+    id   serial PRIMARY KEY,
+    name varchar(64)
+);
+
+CREATE TABLE teachers
+(
+    id   serial PRIMARY KEY,
+    name varchar(64)
+);
+
+CREATE TABLE students_to_teachers_to_subjects
+(
+    teacher_id int REFERENCES teachers (id),
+    student_id int REFERENCES students (id),
+    subject    varchar(300),
+    PRIMARY KEY (teacher_id, student_id)
+);
+
+INSERT INTO students_to_teachers_to_subjects
+VALUES (1, 1, 'biology'),
+       (1, 2, 'biology'),
+       (2, 1, 'math'),
+       (2, 2, 'biology');
+----- <---- this is a problem
+
+--
+-- DO IT RIGHT:
+--
+
+CREATE TABLE students
+(
+    id   serial PRIMARY KEY,
+    name varchar(64)
+);
+
+CREATE TABLE teachers
+(
+    id      serial PRIMARY KEY,
+    name    varchar(64),
+    subject varchar(64) REFERENCES subjects (name)
+);
+
+CREATE TABLE subjects
+(
+    name varchar(64) PRIMARY KEY
+);
+
+CREATE TABLE students_to_teachers
+(
+    teacher_id int REFERENCES teachers (id),
+    student_id int REFERENCES students (id),
+    PRIMARY KEY (teacher_id, student_id)
+);
+
+INSERT INTO students_to_teachers
+VALUES (1, 1),
+       (1, 2),
+       (2, 1),
+       (2, 2);
+
+/*
+Single source of truth
+Need to store primary facts in tables that we cannot know from other facts
+*/
