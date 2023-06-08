76ba9e9e8e4749b97f13209cb61fc6e6ca98ef74
ElyaDzhy <elmira.freshcode@gmail.com>
2023-06-08 10:47:09 +0300

    Add example of 3NF

--- sql/normalization.sql
+++ sql/normalization.sql
@@ -52,3 +52,49 @@ SELECT *
 FROM employees
          JOIN positions ON employees.position = positions.name;
 
+-- 3NF
+
+-- NOT IN 3NF
+--      ||
+--      \/
+
+CREATE TABLE employees
+(
+    id               serial PRIMARY KEY,
+    name             varchar(64),
+    department       varchar(64),
+    phone_department int
+);
+INSERT INTO employees (name, department, phone_department)
+VALUES ('John Doe', 'HR', 66515),
+       ('Jane Smith', 'Sales', 66515),
+       ('Andrew', 'HR', 2232);
+
+/*
+department -> id
+phone_department -> department
+
+transitive dependency ----- NOT IN 3NF ------ phone_department -> id
+*/
+
+--
+-- DO IT RIGHT:
+--
+
+CREATE TABLE departments
+(
+    name  varchar(64) PRIMARY KEY,
+    phone int
+);
+
+CREATE TABLE employees
+(
+    id         serial PRIMARY KEY,
+    name       varchar(64),
+    department varchar(64) REFERENCES departments (name)
+);
+
+INSERT INTO employees (name, department)
+VALUES ('John Doe', 'HR'),
+       ('Jane Smith', 'Sales'),
+       ('Andrew', 'HR');
